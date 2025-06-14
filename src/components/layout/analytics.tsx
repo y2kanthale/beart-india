3362cb378388437cfb5307b09d7674af04f1d77f
Samarth-Kanthale <samarth.beart@gmail.com>
2025-06-14 12:31:31 +0530

    Create analytics.tsx

---
+++ src/components/layout/analytics.tsx
@@ -0,0 +1,26 @@
+//src/components/layout/analytics.tsx
+
+'use client';
+
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-ZXRGN7DE9B'; // Replace with yours
+
+export default function Analytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
